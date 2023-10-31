3d6af10726284e63fefea38e7f6acc86a5d9b741
Nursultan <tupoukidala77@gmail.com>
2023-10-31 20:21:59 +0600

    first commit

---
+++ script.js
@@ -0,0 +1,38 @@
+////1
+//var arr = [1, 2, 3, 0, 4, 5, 6];
+
+//console.log(arr.map(el => el *2));
+////2
+// let arr = [1, 2, 3, 1, 4, 5, 6];
+// let arr = [1, 2, 3, 0, 4, 5, 6];
+//
+// let ta = arr.every(function(el) {
+//     return el > 0;
+// });
+//
+// if (ta) {
+//     console.log("Все элементы в массиве больше нуля.");
+// } else {
+//     console.log("Не все элементы в массиве больше нуля.");
+// }
+////3
+// let arr = [1, 2, -3, 0, 4, 5, -6];
+//
+// let fa = arr.filter(function(num) {
+//     return num < 0;
+// });
+//
+// console.log(fa);
+////4
+// let arr =[1, -2, 3, 0, 4, -5, 6]
+// let fa =arr.filter(function (num){
+//     return num % 2 === 0
+//     })
+// console.log(fa)
+////5
+// var arr = ['hello', 'sit', 'ipsum', 'javascript', 'python', 'sun', 'even'];
+// let fa = arr.filter(function(str) {
+//     return str.length > 5;
+// });
+// console.log(fa)
+////6
